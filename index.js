51722238cde51609b572685e66818b07ca1015c2
code-mira <92872617+code-mira@users.noreply.github.com>
2022-03-20 15:00:12 +0530

    Initial files for project

---
+++ index.js
@@ -0,0 +1,61 @@
+let leng = document.querySelectorAll(".drum").length;
+
+for(let i = 0; i < leng; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        
+        let letter = this.innerHTML;
+        getSound(letter);
+        buttonAnimation(letter);
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    getSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function getSound(letter){
+    switch(letter){
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        
+        default:
+            console.log(letter);
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    let activeButton = document.querySelector('.'+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
